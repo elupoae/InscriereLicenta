2955496908cf10e79d573b192a71715f3245d3bb
Eduard Lupoae <edward_v98@yahoo.com>
2019-04-12 21:29:31 +0300

    Update responsiv
    Am facut pagina admin-dashboard responsiv iar pentru idex (main-page) am adaugat cateva lucruril mici momentan.

---
+++ docs/modul-frontend/Front/javascript/responsive-dashboard.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+   $(".close-bar").click(function (){
+      $("#container .left-bar").slideToggle('fast');
+      $("")
+   });
+   window.onresize = function(event) {
+       if($(window).width() > 650){
+           $("#container .left-bar").show();
+       }
+   }
+
+});
