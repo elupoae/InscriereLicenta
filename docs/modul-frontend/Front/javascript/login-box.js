ba04a27c23ef1c7a14e32db57f8b8ecd97077439
Eduard Lupoae <edward_v98@yahoo.com>
2019-04-21 18:16:03 +0300

    Dashboard: CreateNewAccount si EditAccount

--- docs/modul-frontend/Front/javascript/login-box.js
+++ docs/modul-frontend/Front/javascript/login-box.js
@@ -1,7 +1,10 @@
 let modal_login = document.getElementById('login');
+let modal_add_account = document.getElementById('add-account');
 
 window.onclick = function(event) {
     if (event.target === modal_login) {
         modal_login.style.display = "none";
+    } else if (event.target === modal_add_account) {
+        modal_add_account.style.display = "none"
     }
 };
